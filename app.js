1d39133c799e44235379c24d2e1cae506a8e2777
CB_ATL_118_Noah_Goodman <cb_atl_118_noah_goodman@MacBook-Air-2.local>
2018-10-15 13:43:08 -0400

    writting new fuction for my lives

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+var guesses = 5
+
 //making an array for the words that i want to use in my game
 let answers = ["coffe", "creamer", "mug", "espresso", "beans", "decaf", "barista"]
 
@@ -44,6 +46,9 @@ function answer(letter) {
     for (let i = 0; i < chosenWord.length; i++) {
         if (chosenWord[i] === letter) {
             underScoreArray[i] = letter
+        }else{
+            guesses -= 1
+            if (guesses = 0 )
         }
     }
 
