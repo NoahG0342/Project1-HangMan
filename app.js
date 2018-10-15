235e74e430b09ad7edf5a30bacbea396853c0257
CB_ATL_118_Noah_Goodman <cb_atl_118_noah_goodman@MacBook-Air-2.local>
2018-10-15 10:27:13 -0400

    added some comments to my JS page

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 
-
+//making an array for the words that i want to use in my game
 let answers = ["coffe", "creamer", "mug", "espresso", "beans", "decaf", "barista"]
 
+//building a way to get a new word in my array
 let randomNum = Math.floor(Math.random() * answers.length)
 let chosenWord = answers[randomNum]
+
+//putting the word that is chosin into an array of the letters in the array
 chosenWord = chosenWord.split('')
 let underScoreArray = []
 
+//how i get each letter into the empty array
 for (let i = 0; i < chosenWord.length; i++) {
     underScoreArray.push('_ ')
 }
-
+// how i put the words in the box 
 document.querySelector('.answer-container').innerHTML = underScoreArray.join('')
 
 const buttons = document.querySelectorAll('.button')
 
-
+// making it where all my buttons that i click gome out lowwer case and not uppercase 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function() {
         console.log(buttons[i].value.toLowerCase())
     })
 }
 
-console.log(chosenWord)
-console.log(underScoreArray)
